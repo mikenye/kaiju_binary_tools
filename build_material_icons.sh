b215da8e757d846ec50b868b0ca92c7ad5f8c34a
Mike Nye <mike.nye@gmail.com>
2026-02-23 11:43:42 +0800

    ensure script works when called from different directory

--- build_material_icons.sh
+++ build_material_icons.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -oe pipefail
+set -euo pipefail
+
+# cd into script dir
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+cd "$SCRIPT_DIR"
 
 # get repository root directory on local system
 REPO_ROOT="$(git rev-parse --show-superproject-working-tree)"
